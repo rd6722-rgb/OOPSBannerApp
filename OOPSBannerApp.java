c0875b2597a9559170c8e4360393789add5f2b70
RIDDHIRAJ <rd67222@srmist.edu.in>
2026-03-01 01:17:21 +0530

    ADDED UC3

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,15 +1,14 @@
 public class OOPSBannerApp {
-
     public static void main(String[] args) {
-        System.out.println("  *** " + "   *** " + " ***** " + "  **** ");
-        System.out.println(" *   *" + "  *   *" + " *    *" + " * ");
-        System.out.println(" *   *" + "  *   *" + " *    *" + " * ");
-        System.out.println(" *   *" + "  *   *" + " ***** " + "  *** ");
-        System.out.println(" *   *" + "  *   *" + " * " + "          *");
-        System.out.println(" *   *" + "  *   *" + " * " + "          *");
-        System.out.println("  *** " + "   *** " + " * " + "      **** ");
-        //done
-    
+        // UC3 Goal: Use String.join() with an empty delimiter "" [cite: 158, 161]
+        System.out.println(String.join("", "  *** ", "   *** ", " ***** ", "  **** "));
+        System.out.println(String.join("", " *   * ", " *   * ", "*   * ", " * "));
+        System.out.println(String.join("", " *   * ", " *   * ", "*   * ", " * "));
+        System.out.println(String.join("", " *   * ", " *   * ", "***** ", "  *** "));
+        System.out.println(String.join("", " *   * ", " *   * ", "* ", "         * "));
+        System.out.println(String.join("", " *   * ", " *   * ", "* ", "         * "));
+        System.out.println(String.join("", "  *** ", "   *** ", " * ", "     **** "));
     }
 }
+
 
