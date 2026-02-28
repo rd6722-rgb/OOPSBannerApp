6a98ddf76a2806f3d43749297d0377b6e53b96a5
RIDDHIRAJ <rd67222@srmist.edu.in>
2026-03-01 01:22:08 +0530

    ADDED UC4

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,14 +1,24 @@
 public class OOPSBannerApp {
+
     public static void main(String[] args) {
-        // UC3 Goal: Use String.join() with an empty delimiter "" [cite: 158, 161]
-        System.out.println(String.join("", "  *** ", "   *** ", " ***** ", "  **** "));
-        System.out.println(String.join("", " *   * ", " *   * ", "*   * ", " * "));
-        System.out.println(String.join("", " *   * ", " *   * ", "*   * ", " * "));
-        System.out.println(String.join("", " *   * ", " *   * ", "***** ", "  *** "));
-        System.out.println(String.join("", " *   * ", " *   * ", "* ", "         * "));
-        System.out.println(String.join("", " *   * ", " *   * ", "* ", "         * "));
-        System.out.println(String.join("", "  *** ", "   *** ", " * ", "     **** "));
+        // 1. Create a String array to store all 7 lines of the banner
+        String[] banner = new String[7];
+
+        // 2. Populate the array using String.join to construct each line
+        banner[0] = String.join("", "  *** ", "   *** ", " ***** ", "  **** ");
+        banner[1] = String.join("", " *   * ", " *   * ", "*    * ", " * ");
+        banner[2] = String.join("", " *   * ", " *   * ", "*    * ", " * ");
+        banner[3] = String.join("", " *   * ", " *   * ", "***** ", "   *** ");
+        banner[4] = String.join("", " *   * ", " *   * ", "* ", "          * ");
+        banner[5] = String.join("", " *   * ", " *   * ", "* ", "          * ");
+        banner[6] = String.join("", "  *** ", "   *** ", " * ", "      **** ");
+
+        // 3. Use an Enhanced For-Loop to iterate through the array and print each line
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 }
+
 
 
